80e2606a760c11849a40ddb98993a2b01e4bafb8
soojjung <89066676+soojjung@users.noreply.github.com>
2024-06-20 18:04:45 +0900

    feat: basic 과제

--- packages/assignment-1/src/basic/RequireRefactoring.tsx
+++ packages/assignment-1/src/basic/RequireRefactoring.tsx
@@ -2,22 +2,32 @@ import { ComponentProps, memo, PropsWithChildren } from "react";
 
 type Props = {
   countRendering?: () => void;
-}
+};
 
-const PureComponent = memo(({ children, countRendering, ...props }: PropsWithChildren<ComponentProps<'div'> & Props>) => {
-  countRendering?.();
-  return <div {...props}>{children}</div>
-})
+const PureComponent = memo(
+  ({
+    children,
+    countRendering,
+    ...props
+  }: PropsWithChildren<ComponentProps<"div"> & Props>) => {
+    countRendering?.();
+    return <div {...props}>{children}</div>;
+  }
+);
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-let outerCount = 1
+let outerCount = 1;
+const style = { width: "100px", height: "100px" };
+const onClick = () => {
+  outerCount += 1;
+};
 
 // useMemo, useCallback 등을 사용하지 않고 이 컴포넌트를 개선해보세요.
 export default function RequireRefactoring({ countRendering }: Props) {
   return (
     <PureComponent
-      style={{ width: '100px', height: '100px' }}
-      onClick={() => {outerCount += 1;}}
+      style={style}
+      onClick={onClick}
       countRendering={countRendering}
     >
       test component
--- packages/assignment-1/src/basic/UseCallbackTest.tsx
+++ packages/assignment-1/src/basic/UseCallbackTest.tsx
@@ -1,16 +1,24 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import { BarkButton, MeowButton } from "./UseCallbackTest.components.tsx";
 
 export default function UseCallbackTest() {
   const [meowCount, setMeowCount] = useState(0);
   const [barkedCount, setBarkedCount] = useState(0);
 
+  const memoizationMeowCallback = useCallback(() => {
+    setMeowCount((n) => n + 1);
+  }, []);
+
+  const memoizationBarkedCallback = useCallback(() => {
+    setBarkedCount((n) => n + 1);
+  }, []);
+
   return (
     <div>
       <p data-testid="cat">meowCount {meowCount}</p>
       <p data-testid="dog">barkedCount {barkedCount}</p>
-      <MeowButton onClick={() => setMeowCount(n => n + 1)}/>
-      <BarkButton onClick={() => setBarkedCount(n => n + 1)}/>
+      <MeowButton onClick={memoizationMeowCallback} />
+      <BarkButton onClick={memoizationBarkedCallback} />
     </div>
   );
 }
