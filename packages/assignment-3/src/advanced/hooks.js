6e6e99e3c0c1c87faa8f080cf7c234f5ce51f3fe
soojjung <89066676+soojjung@users.noreply.github.com>
2024-07-05 00:56:49 +0900

    feat: 3주차 advanced 과제

--- packages/assignment-3/src/advanced/hooks.js
+++ packages/assignment-3/src/advanced/hooks.js
@@ -1,15 +1,66 @@
 export function createHooks(callback) {
+  const states = [];
+  let stateIndex = 0;
+  let rAFId = null;
+
+  const memos = [];
+  let memoIndex = 0;
+
   const useState = (initState) => {
-    return [];
+    const currentIndex = stateIndex;
+
+    if (states[stateIndex] === undefined) {
+      states[stateIndex] = initState;
+    }
+
+    const state = states[currentIndex];
+
+    const setState = (newValue) => {
+      if (!deepEquals(states[currentIndex], newValue)) {
+        states[currentIndex] = newValue;
+
+        if (rAFId === null) {
+          rAFId = requestAnimationFrame(() => {
+            if (typeof callback === "function") {
+              callback();
+            }
+            rAFId = null;
+          });
+        }
+      }
+    };
+
+    stateIndex += 1;
+    return [state, setState];
   };
 
   const useMemo = (fn, refs) => {
-    return fn();
+    const currentIndex = memoIndex;
+    if (!memos[currentIndex] || !deepEquals(memos[currentIndex].deps, refs)) {
+      memos[currentIndex] = { value: fn(), deps: refs };
+    }
+
+    memoIndex += 1;
+    return memos[currentIndex].value;
   };
 
   const resetContext = () => {
-
-  }
+    stateIndex = 0;
+    memoIndex = 0;
+  };
 
   return { useState, useMemo, resetContext };
+}
+
+function deepEquals(a, b) {
+  if (a === b) return true;
+  if (typeof a !== "object" || typeof b !== "object" || a == null || b == null)
+    return false;
+  const keysA = Object.keys(a),
+    keysB = Object.keys(b);
+  if (keysA.length !== keysB.length) return false;
+  for (const key of keysA) {
+    if (!keysB.includes(key) || !deepEquals(a[key], b[key])) return false;
+  }
+  return true;
 }
