3955c1872e320ff16a59e6307a2363a310794efe
soojjung <89066676+soojjung@users.noreply.github.com>
2024-08-21 04:49:36 +0900

    Update server.tsx

--- packages/assignment-10/src/advanced/server.tsx
+++ packages/assignment-10/src/advanced/server.tsx
@@ -1,17 +1,17 @@
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
-import React from 'react';
-import express from 'express';
-import ReactDOMServer from 'react-dom/server';
-import { App } from './App.tsx';
+import React from "react";
+import http from "http";
+import { renderToPipeableStream } from "react-dom/server";
+import { App } from "./App.tsx";
+import zlib from "zlib";
 
-const app = express();
 const port = 3333;
 
-app.get('*', (req, res) => {
-  const app = ReactDOMServer.renderToString(<App url={req.url}/>);
+const cache = new Map<string, Buffer>();
 
-  res.send(`
+function htmlTemplate(appHtml: string): string {
+  return `
     <!DOCTYPE html>
     <html lang="en">
     <head>
@@ -20,12 +20,53 @@ app.get('*', (req, res) => {
       <title>Simple SSR</title>
     </head>
     <body>
-      <div id="root">${app}</div>
+      <div id="root">${appHtml}</div>
     </body>
     </html>
-  `);
-});
+  `;
+}
 
-app.listen(port, () => {
+const requestListener = (
+  req: http.IncomingMessage,
+  res: http.ServerResponse
+) => {
+  const url = req.url || "/";
+
+  if (cache.has(url)) {
+    res.setHeader("Content-Type", "text/html");
+    res.setHeader("Content-Encoding", "gzip");
+    return res.end(cache.get(url));
+  }
+
+  const stream = renderToPipeableStream(<App url={url} />, {
+    onShellReady() {
+      res.setHeader("Content-Type", "text/html");
+      res.setHeader("Content-Encoding", "gzip");
+
+      const gzip = zlib.createGzip();
+      const chunks: Buffer[] = [];
+
+      gzip.on("data", (chunk) => chunks.push(chunk));
+
+      gzip.on("end", () => {
+        const fullResponse = Buffer.concat(chunks);
+        cache.set(url, fullResponse);
+        res.end(fullResponse); // 최종 응답 전송
+      });
+
+      const fullHtml = htmlTemplate('<div id="root"></div>');
+      stream.pipe(gzip).end(fullHtml);
+    },
+    onError(err) {
+      console.error(err);
+      res.statusCode = 500;
+      res.end("Internal Server Error");
+    },
+  });
+};
+
+const server = http.createServer(requestListener);
+
+server.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
